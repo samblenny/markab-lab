cf89dba60b6eae07655903d7bcb00f6ebbbbb17e
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2022-05-18 22:30:01 +0000

    Revise tests for nested calls

--- repl/test/colon_defs.fs
+++ repl/test/colon_defs.fs
@@ -15,18 +15,20 @@
 ( 2147483647 65535 255 1 0  OK)     decimal clearstack h
 ( 2147483647 65535 255 1 0  OK)             clearstack i
 ( --- Test nested function calls ---)
-(  OK)    : fib swap over + dup . ;
-(  OK)                 : a 1 1 .s ;
-(  OK)            : b fib fib fib ;
-(  OK)     : c clearstack a b b b ;
-( 1 1 2 3 5 8 13 21 34 55 89  OK) c
+(  OK)       : f0 swap dup . over + ;
+(  OK)                : f1 f0 f0 f0 ;
+(  OK)       : fib 1 dup f1 f1 f1 . ;
+( 1 1 2 3 5 8 13 21 34 55 89  OK) fib
 clearstack
+( --- Test implicit tail call ---)
+(  OK) : start 1 .  : continue 2 . 3 . ;
+( 1 2 3  OK)                       start
+( 2 3  OK)                      continue
 ( --- Test maxing out return stack ---)
-(  OK)   : f1 0  : f2 1 + 42 emit f2 ;
-( ***************** E21...)         f1
-( * E21...)                         f1
-(  OK)                     clearreturn
-( ***************** E21...)         f1
-(  OK)                     clearreturn
-(  17 1 17  OK)                     .s
+(  OK)     : f1 0  : f2 1 + 42 emit f2 ;
+( ***************** E21...)           f1
+( * E21...)                           f1
+clearreturn
+( ***************** E21...)           f1
+(  17 1 17  OK)           clearreturn .s
 clearstack
