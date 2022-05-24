1a00debb5a2363ca5fce2680a9ab8aa2b78d9824
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2022-05-24 02:03:36 +0000

    Capture weird return stack behavior with tests
    Probably I need to make a new dictionary for compile mode and move
    `>r`, `r>`, `next`, and friends to that dictionary. There also
    appears to be a bug that's allowing a cascade of error messages.

---
+++ repl/test/return_stack.fs
@@ -0,0 +1,10 @@
+( === tests/return_stack.fs ===)
+(  OK)  : a 1 >r 2 >r 3 >r .ret r> r> r> ;
+(  1 2 3  OK)                            a
+(  OK)                : broken 1 >r .ret ;
+(  E19 Bad code pointer)            broken
+( ---------------- TODO: fix this!  ^^^^^)
+(   Stack is empty  OK)               .ret
+
+( >r should not be defined when not compiling)  1 >r
+( ------------------------ TODO: also fix this! ^^^)
