d185e672917bb161f6ee9ad1440867e26af29fec
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2022-06-05 09:23:04 +0000

    Sketching in rough ideas for new VM design

--- repl2/bootstrap.py
+++ repl2/bootstrap.py
@@ -7,7 +7,97 @@
 
 ROM_FILE = 'kernel.bin'
 
+TOKENS = """
+nop Nop
+& And
+b@ ByteFetch
+b! ByteStore
+/% DivMod
+drop Drop
+dup Dup
+= Equal
+@ Fetch
+> Greater
+~ Invert
+< Less
+- Minus
+* Mul
+<> NotEq
+| Or
+over Over
++ Plus
+reset Reset
+r> RFrom
+! Store
+swap Swap
+>r ToR
+w@ WordFetch
+w! WordStore
+^ Xor
+0= ZeroEqual
+"""
+
+MEMORY_MAP = """
+0000 IO       # Memory mapped IO area           64 bytes
+003F IOEnd    # End memory mapped IO area
+#... Free0    # Unallocated
+0050 T        # Top of data stack               4 bytes
+0054 S        # Second on data stack            4 bytes
+0058 R        # top of Return stack             4 bytes
+005C A        # Accumulator (address register)  4 bytes
+0060 DStack   # Data Stack                      16 * 4 bytes
+00A0 RStack   # Return Stack                    16 * 4 bytes
+00E0 IP       # Instruction Pointer             2 bytes
+00E2 IN       # INput buffer INdex              2 bytes
+00E4 DP       # Dictionary Pointer              2 bytes
+00E6 IBPtr    # Input Buffer Pointer            2 bytes
+00E8 IBLen    # Input Buffer Length             2 bytes
+00EA Fence    # Fence (write-protect for !)     2 bytes
+00EC Free1    # Unallocated
+#...
+0400 Heap     # Heap (dictionary)               54 KB
+DC00 HeapRes  # Heap Reserved area (for WORD)   1 KB
+DFFF HeapMax
+E000 TIB      # Terminal Input Buffer           1 KB
+E3FF TIBMax
+E400 BLK      # BLocK buffer                    1 KB
+E7FF BLKMax
+E800 Pad      # Pad buffer                      1 KB
+EBFF PadMax
+EC00 Fmt      # Format buffer                   1 KB
+EFFF FmtMax   # End of format buffer
+F000 Free2    # Unallocated
+#...
+FFFF MemMax
+"""
+
+def make_constants():
+  constants = []
+  words = []
+  lines = TOKENS.strip().split("\n")
+  for (i, line) in enumerate(lines):
+    (name, opcode) = line.strip().split(" ")
+    constants += [f"{i:2} const {opcode}"]
+    words += [f": {name:5} tok> {opcode} ;"]
+  result = constants
+  result += words
+  return "\n".join(result)
+
+def make_memory_map():
+  lines = MEMORY_MAP.strip().split("\n")
+  lines = [L.split("#")[0].strip() for L in lines]    # filter comments
+  lines = [L for L in lines if len(L) > 0]            # filter empty lines
+  constants = ["hex"]
+  for line in lines:
+    (addr, name) = line.split(" ")
+    constants += [f"{addr} const {name}"]
+  constants += ["decimal"]
+  return "\n".join(constants)
+
 with open(ROM_FILE, 'w') as f:
-  f.write(0)
+  f.write("")
 
 print("TODO: write a bootstrap compiler")
+
+print(make_constants())
+print(make_memory_map())
--- repl2/markab_library.fs
+++ repl2/markab_library.fs
@@ -2,10 +2,70 @@
 ( SPDX-License-Identifier: MIT)
 ( MarkabForth library: all the stuff that runs on top of the kernel)
 
+: asm8  here [ ByteStore ] ;  ( compile low byte of T as a U8)
+: asm16 here [ WordStore ] ;  ( compile low word of T as U16)
+: asm32 here [ Store     ] ;  ( compile double word T as U32)
+: @  Fetch asm8 ;
+: !  Store asm8 ;
+: w@ WordFetch asm8 ;
+: w! WordStore asm8 ;
+: b@ ByteFetch asm8 ;
+: b! ByteStore asm8 ;
+: +  Plus asm8 ;
+: -  Minus asm8 ;
+: *  Mul asm8 ;
+: /  Div asm8 ;
+: %  Mod asm8 ;
+: /% DivMod asm8 ;
+: &  And asm8 ;
+: |  Or asm8 ;
+: ^  Xor asm8 ;
+
+
 : outer ( outer interpreter) ;
-: find ( find word in dictionary ) ;
-: if  ... does> ... ;
-: else ... does> ... ;
-: ;if ... does> ... ;
-: for ... does> ... ;
-: ;for ... does> ... ;
+: follow @ ;
+: find current dup 0= if ret endif w@ ;
+: if   ;
+: else  ;
+: ;if   ;
+: for here  ;
+: ;for  ;
+: .  ;
+: hex decimal does> 16 base ! ;
+: decimal decimal does> 10 base ! ;
+
+decimal
+?? const TpVar
+?? const TpConst
+?? const TpCode
+?? const TpToken
+?? const Heap
+65535 const HeapEnd
+256 const HReserve
+var DP
+var Base
+var Pad
+var Blk
+var TIB
+var CallDP
+var Context
+var Current
+var Last
+
+: here dp @ ;
+: allot here + dp ! ;
+: , here 4 allot ! ;
+: ? @ . ;
+: setType here b! 1 allot ;
+: setParam here ! 4 allot ;
+: setHead last w! ;
+: var here create TpVar setType 0 ( init to 0) setParam setHead ;
+: const here swap create TpConst setType ( T: n) setParam setHead ;
+
+: ." ... does> ... ;
+: version ." markabForth v0.2.0" ;
+const
+: init-vars ?? Pad ! ?? Blk ! ?? TIB ! ?? CallDP ! Heap Base ;
+: cold init-vars version ... ;
+: doword type? case 0 doToken 1 doCode 2 doConst 3 doVar endcase ;
+: interpret word find if doword interpret ret endif ;
