8cee7ac776e2a2e70065121fda8c1a12bea4b83a
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2022-07-23 04:32:40 +0000

    Refactor pdbot as first step to 3-way conversation
    I'm trying to figure out how it should work for me to have a 3-way
    conversation with mkbot and pdbot. Having separate pd and mkb
    channels feels weird. I think it would be better to have everybody
    on mkb listening for messages addressing them by name, then we can
    use queries for one on one conversations.
    
    This commit includes some filtering so pdbot will respond to either
    general greetings or being directly addressed in the main channel.
    But, in a query, pdbot will assume all messages other than
    greetings are meant for them. This provides a path to experiment
    with messages to Pd without having to always write the direct
    address prefix at the start of every line.

---
+++ repl2/ircbot/__init__.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2022 Sam Blenny
+# SPDX-License-Identifier: MIT
+"""
+Irc is a class to handle the irc messaging portion of implementing an irc bot.
+"""
+import asyncio
+import random
+import re
+
+class Irc():
+  def __init__(self, nick, name, host, server, port, chan):
+    """Initialize irc connection details and message filters"""
+    self.nick = nick
+    self.name = name
+    self.host = host
+    self.server = server
+    self.port = port
+    self.chan = chan
+    self.reader = None
+    self.writer = None
+    self.rx_callback = None
+    self.rx_irq = None
+    # Prepare regular expressions for use in routing inbound messages
+    re_privmsg = f":([^ ]+)![^ ]+ PRIVMSG ({chan}|{nick}) :(.*)"
+    re_greet = f"([Hh](i|ello))[^a-zA-Z_-]*$"
+    nick2 = nick[:2]
+    re_to_me = f"(@{nick}|@{nick2}|{nick}[,:]|{nick2}[,:]) *(.*)"
+    self.re_privmsg = re.compile(re_privmsg)
+    self.re_greet = re.compile(re_greet)
+    self.re_to_me = re.compile(re_to_me)
+
+  async def route(self, line):
+    """Route incoming messages to a suitable handler"""
+    # Route 0: Message is not a PRIVMSG in our channel or query -> Ignore it
+    re_privmsg = self.re_privmsg.match(line)
+    if not re_privmsg:
+      return
+    # PRIVMSG routing considers sender, channel, and body contents
+    (sender, channick, body) = re_privmsg.group(1, 2, 3)
+    # Route 1: General greeting in channel or query -> Answer it
+    re_greet = self.re_greet.match(body)
+    if re_greet:
+      salutation = random.choice(['hi', 'hello', 'Hi', 'Hello'])
+      pause = random.normalvariate(0.6, 0.1)
+      await asyncio.sleep(pause)
+      if channick == self.nick:
+        # In a query addressed to me, reply should be addressed to sender
+        channick = sender
+      await self.notice(channick, f"{salutation}, {sender}")
+      return
+    # Route 2: Query -> Forward message body to backend
+    if channick == self.nick:
+      await self.backend.send(body, channick)
+      return
+    # Route 3: Addressed in channel -> Forward tail of message body to backend
+    re_to_me = self.re_to_me.match(body)
+    if (channick == self.chan) and re_to_me:
+      tail = re_to_me.group(2)
+      await self.backend.send(tail, channick)
+      return
+
+  def set_backend(self, sender):
+    """Register a backend sender object that provides a send(str) method"""
+    self.backend = sender
+
+  async def connect(self):
+    """Connect to the irc server"""
+    print(f">>> connecting to irc {self.server}:{self.port} >>>")
+    retry = 5
+    notice_enable = True    # <- arm the one-shot error message mechanism
+    while True:
+      try:
+        # When the server is up and listening, this should work right away
+        reader, writer = await asyncio.open_connection(self.server, self.port)
+        self.reader = reader
+        self.writer = writer
+        await self.send(f"NICK {self.nick}")
+        user = f"USER {self.nick} {self.host} {self.server} :{self.name}"
+        await self.send(user)
+        await self.writer.drain()
+        break
+      except OSError:
+        # If something went wrong, auto-retry with delay
+        if notice_enable:
+          print(">>> irc connect failed: check irc server >>>")
+          # Disable further retry notifications to avoid log spam
+          notice_enable = False
+        await asyncio.sleep(retry)
+
+  async def join(self):
+    """Join our irc channel"""
+    await self.send(f"JOIN {self.chan}")
+
+  async def listen(self):
+    """Start the event loop to listen for, and respond to, inbound messages"""
+    while True:
+      if self.reader is None or self.reader.at_eof():
+        # connection is down, so try to reconnect
+        await self.connect()
+        await self.join()
+      line = await self.reader.readline()
+      line = line.decode('utf8').strip()
+      if line == '':
+        await asyncio.sleep(0.001)
+      else:
+        if line.startswith("PING "):
+          await self.send(f"PONG {line[5:]}")
+        else:
+          print(line)
+          await self.route(line)
+
+  async def notice(self, chan, message):
+    """Send an irc NOTICE (most bot messages should generally use this)"""
+    if not chan:
+      chan = self.chan
+    await self.send(f"NOTICE {chan} :{message}")
+
+  async def privmsg(self, chan, message):
+    """Send an irc PRIVMSG (CAUTION! using this can create feedback loops)"""
+    if not chan:
+      chan = self.chan
+    await self.send(f"PRIVMSG {chan} :{message}")
+
+  async def send(self, message):
+    """Send irc message with crude truncation at 480 bytes.
+    The RFC limit is 512 bytes for a message, but ngircd will cut messages down
+    to about 497 bytes. So, I will stay a bit below the lower limit. This way
+    of truncation can misbehave badly for long messages that include non-ASCII
+    characters. If that becomes an issue, the truncation routine needs to be
+    improved to detect Unicode grapheme cluster boundaries. That's a fairly
+    involved thing to do, so for now I'll just igore it and hope for the best.
+    """
+    message = f"{message}".encode('utf8')
+    if len(message) > 480:
+      message = message[:480] + "[TRUNCATED]\r\n".encode('utf8')
+    else:
+      message += "\r\n".encode('utf8')
+    if self.writer is None or self.writer.is_closing():
+      print(">>> irc is disconnected >>>")
+    else:
+      print(message.decode('utf8').strip())
+      self.writer.write(message)
+      await self.writer.drain()
--- repl2/pdbot/__init__.py
+++ repl2/pdbot/__init__.py
@@ -6,107 +6,24 @@ Pdbot is an irc bot to connect pd-vanilla netsend/netreceive objects to IRC.
 To start pdbot, run `python3 -m pdbot` from a terminal.
 """
 import asyncio
+import random
 import re
 
-class Irc():
-  def __init__(self, nick, name, host, server, port, chan):
-    self.nick = nick
-    self.name = name
-    self.host = host
-    self.server = server
-    self.port = port
-    self.chan = chan
-    self.reader = None
-    self.writer = None
-    # Regular expression to match PRIVMSG or NOTICE messages in {chan}
-    pattern = f":([^ ]+)![^ ]+ (PRIVMSG|NOTICE) {chan} :(.*)"
-    self.wake = re.compile(pattern)
-
-  def set_pd(self, pd):
-    self.pd = pd
-
-  async def connect(self):
-    print(f">>> connecting to irc {self.server}:{self.port} >>>")
-    retry = 5
-    notice_enable = True
-    while True:
-      try:
-        # if the server is up and listening, this should work right away
-        reader, writer = await asyncio.open_connection(self.server, self.port)
-        self.reader = reader
-        self.writer = writer
-        await self.send(f"NICK {self.nick}")
-        user = f"USER {self.nick} {self.host} {self.server} :{self.name}"
-        await self.send(user)
-        await self.writer.drain()
-        break
-      except OSError:
-        # if something went wrong, auto-retry with delay
-        if notice_enable:
-          print(">>> irc connect failed: check irc server >>>")
-          notice_enable = False
-        await asyncio.sleep(retry)
-
-  async def join(self):
-    await self.send(f"JOIN {self.chan}")
-
-  async def listen(self):
-    while True:
-      if self.reader is None or self.reader.at_eof():
-        # connection is down, so try to reconnect
-        await self.connect()
-        await self.join()
-      line = await self.reader.readline()
-      line = line.decode('utf8').strip()
-      if line == '':
-        await asyncio.sleep(0.001)
-      else:
-        print(line)
-        if line.startswith("PING "):
-          await self.pong(line[5:])
-        else:
-          await self.parse(line)
-
-  async def pong(self, dest):
-    await self.send(f"PONG {dest}")
-
-  async def parse(self, line):
-    """Parse input line to see if somebody is talking to the bridge"""
-    result = self.wake.match(line)
-    if result:
-      (sender, method, message) = result.group(1, 2, 3)
-      if method == 'PRIVMSG':
-        if message.lower().startswith("hi"):
-          await self.notice(f"Hello, {sender}")
-        else:
-          await self.pd.send(message)
-
-  async def privmsg(self, message):
-    await self.send(f"PRIVMSG {self.chan} :{message}")
-
-  async def notice(self, message):
-    await self.send(f"NOTICE {self.chan} :{message}")
-
-  async def send(self, message):
-    if self.writer is None or self.writer.is_closing():
-      print(">>> irc is disconnected >>>")
-    else:
-      print(message)
-      self.writer.write(f"{message}\r\n".encode('utf8'))
-      await self.writer.drain()
+from ircbot import Irc
 
 
 class Pd():
-  def __init__(self, server, port):
+  def __init__(self, server, port, irc_chan):
     self.server = server
     self.port = port
+    self.irc_chan = irc_chan
     self.reader = None
     self.writer = None
 
   def set_irc(self, irc):
     self.irc = irc
 
-  async def connect(self):
+  async def connect(self, chan):
     print(f">>> connecting to pd {self.server}:{self.port} >>>")
     retry = 5
     notice_enable = True
@@ -116,12 +33,12 @@ class Pd():
         reader, writer = await asyncio.open_connection(self.server, self.port)
         self.reader = reader
         self.writer = writer
-        await self.irc.notice("[pd is connected]")
+        await self.irc.notice(chan, "[pd is connected]")
         break
       except OSError:
         # if something went wrong, auto-retry with delay
         if notice_enable:
-          await self.irc.notice("[pd is disconnected: check patch]")
+          await self.irc.notice(chan, "[pd is disconnected: check patch]")
           notice_enable = False
         await asyncio.sleep(retry)
 
@@ -129,17 +46,17 @@ class Pd():
     while True:
       if self.reader is None or self.reader.at_eof():
         # connection to pd is down, so try to connect
-        await self.connect()
+        await self.connect(self.irc_chan)
       line = await self.reader.readline()
       line = line.decode('utf8').strip()
       if line == '':
         await asyncio.sleep(0.001)
       else:
-        await self.pd.notice(line)
+        await self.backend.notice(line)
 
-  async def send(self, message):
+  async def send(self, message, channick):
     if self.writer is None or self.writer.is_closing():
-      await self.irc.notice("[pd is disconnected: check patch]")
+      await self.irc.notice(channick, "[pd is disconnected: check patch]")
     else:
       self.writer.write(f"{message}\r\n".encode('utf8'))
       await self.writer.drain()
@@ -151,14 +68,14 @@ async def main():
   host = 'localhost'         # connecting from localhost
   irc_server = 'localhost'   # ...to ngircd server also on localhost
   irc_port = 6667
-  chan = '#pd'
   pd_server = 'localhost'
   pd_port = 3000             # Pd netreceive docs use 3000, seems good?
+  chan = '#mkb'
 
   # Create the connection manager objects and cross-link them
   irc = Irc(nick, name, host, irc_server, irc_port, chan)
-  pd = Pd(pd_server, pd_port)
-  irc.set_pd(pd)
+  pd = Pd(pd_server, pd_port, chan)
+  irc.set_backend(pd)
   pd.set_irc(irc)
 
   # Start the IRC connection
