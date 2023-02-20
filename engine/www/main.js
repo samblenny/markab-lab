8ae0d92e5d6cb53e6936083cba093633ddfbcf0b
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2023-02-20 04:43:52 +0000

    Optimize page load for less jank
    Previously, all of the WebGL shader compiling and program linking
    happened when the main.js module loaded. This commit factors the
    WebGL and Wasm initialization into several functions that get
    spread across a chain of requestAnimationFrame callbacks.
    
    There's also a new `PAUSED` interlock that keeps the main animation
    loop from starting until a gamepadconnected event happens.
    
    Point of all of this is to make the initial page load happen very
    quickly without loading down the main UI thread. It was already
    reasonably fast, but what I'm aiming for here is to be able to run
    okay on firefox-esr on a Pi 3. I want the CPU load to be small.

--- engine/www/main.js
+++ engine/www/main.js
@@ -18,6 +18,7 @@ const gl = SCREEN.getContext('webgl', {
 if(gl === null) {
     console.error("Unable to initialize webgl");
 }
+const GLD = {};  /* Dictionary to hold gl state objects during init chain */
 
 /* WASM module stuff, including shared memory regions */
 const wasmModule = "markab-engine.wasm";
@@ -27,149 +28,192 @@ var FRAME_BUFFER;  /* Wrapper object for shared framebuffer memory region */
 var WIDE = 240;    /* Framebuffer px width */
 var HIGH = 160;    /* Framebuffer px height */
 
-/* Animation timing */
+/* Animation Control */
 var PREV_TIMESTAMP;  /* Timestamp of previous animation frame */
+var PAUSED = true;   /* Controls requestAnimationFrame() looping */
 
 
-/************************************************/
-/* WebGL Shaders for 2D Orthographic Projection */
-/************************************************/
+/******************************************************/
+/* WebGL Shaders for 2D Orthographic Projection       */
+/*                                                    */
+/* Shader compile & link can be slow-ish, so the work */
+/* is split across a chain of requestAnimationFrame   */
+/* callback functions to avoid blocking main thread.  */
+/******************************************************/
 
-/* Clear gl canvas to solid gray */
-gl.clearColor(0.3, 0.3, 0.3, 1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-/* Compile gl vertex shader */
-const vertexSrc =
-`   precision mediump float;
-    attribute vec4 a_position;  // <- assume gl provides .z=0, .w=1
-    void main() {
-        // Scale from tile coords to px coords
-        vec2 pos = a_position.xy * vec2(16.0, 16.0);
-        // Scale and translate from px coords to clip space
-        pos /= vec2(120.0, 80.0);
-        pos -= 1.0;
-        pos *= vec2(1.0, -1.0);
-        gl_Position = vec4(pos, 0.0, 1.0);
-    }
-`;
-var vShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vShader, vertexSrc);
-gl.compileShader(vShader);
-if(!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) {
-    throw "vShader compile log: " + gl.getShaderInfoLog(vShader);
+/* WebGL incremental init chain step 0: Clear canvas */
+function glInit0Clear() {
+    gl.clearColor(0.3, 0.3, 0.3, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    /* Schedule next init function */
+    window.requestAnimationFrame(glInit1VertexShader);
 }
 
-/* Compile gl fragment shader */
-const fragmentSrc =
-`   precision mediump float;
-    void main() {
-        vec2 tile = mod(gl_FragCoord.xy, 16.0);
-        float x = (tile.x < 1.0) || (tile.y < 1.0) ? 0.7 : 1.0;
-        gl_FragColor = vec4(x, 0.0, x, 1.0); /* magenta */
+/* WebGL incremental init chain step 1: Compile vertex shader */
+function glInit1VertexShader() {
+    const vertexSrc =
+    `   precision mediump float;
+        attribute vec4 a_position;  // <- assume gl provides .z=0, .w=1
+        void main() {
+            // Scale from tile coords to px coords
+            vec2 pos = a_position.xy * vec2(16.0, 16.0);
+            // Scale and translate from px coords to clip space
+            pos /= vec2(120.0, 80.0);
+            pos -= 1.0;
+            pos *= vec2(1.0, -1.0);
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    `;
+    GLD.vShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(GLD.vShader, vertexSrc);
+    gl.compileShader(GLD.vShader);
+    if(!gl.getShaderParameter(GLD.vShader, gl.COMPILE_STATUS)) {
+        throw "vShader compile log: " + gl.getShaderInfoLog(GLD.vShader);
     }
-`;
-var fShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fShader, fragmentSrc);
-gl.compileShader(fShader);
-if(!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) {
-    throw "fShader compile log: " + gl.getShaderInfoLog(fShader);
+    /* Schedule next init function */
+    window.requestAnimationFrame(glInit2FragmentShader);
 }
 
-/* Link gl Program */
-const program = gl.createProgram();
-gl.attachShader(program, vShader);
-gl.attachShader(program, fShader);
-gl.linkProgram(program);
-if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw "program link log: " + gl.getProgramInfoLog(program);
+/* WebGL incremental init chain step 2: Compile fragment shader */
+function glInit2FragmentShader() {
+    const fragmentSrc =
+    `   precision mediump float;
+        void main() {
+            vec2 tile = mod(gl_FragCoord.xy, 16.0);
+            float x = (tile.x < 1.0) || (tile.y < 1.0) ? 0.7 : 1.0;
+            gl_FragColor = vec4(x, 0.0, x, 1.0); /* magenta */
+        }
+    `;
+    GLD.fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(GLD.fShader, fragmentSrc);
+    gl.compileShader(GLD.fShader);
+    if(!gl.getShaderParameter(GLD.fShader, gl.COMPILE_STATUS)) {
+        throw "fShader compile log: " + gl.getShaderInfoLog(GLD.fShader);
+    }
+    /* Schedule next init function */
+    window.requestAnimationFrame(glInit3Program);
 }
 
-/* Start using the gl program */
-gl.useProgram(program);
-
-/* Release memory used to compile shaders */
-/* developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices */
-gl.deleteShader(vShader);
-gl.deleteShader(fShader);
-gl.detachShader(program, vShader);
-gl.detachShader(program, fShader);
-vShader = undefined;
-fShader = undefined;
-
-/* Add an indexed buffer of triangle strips to tile the screen          */
-/* - NDC coordinates are left-handed: +x=right +y=up +z=into_screen     */
-/* - Default front-face winding order is counter-clockwise              */
-/* - Triangle strip winding order is determined by first triangle       */
-/* - To append disjoint strips A and B into a single buffer, repeat the */
-/*   last vertex of A and first vertex of B to create a "degenerate"    */
-/*   triangle that marks the start of a new triangle strip.             */
-const columns = 240/16;
-const rows = 160/16;
-const vertices = new Float32Array((columns + 1) * (rows + 1) * 2);
-for(let y = 0; y <= rows; y++) {
-    for(let x = 0; x <= columns; x++) {
-        let i = ((y * (columns + 1)) + x) * 2;
-        vertices[i] = x;
-        vertices[i+1] = y;
+/* WebGL incremental init chain step 3: Link and use program */
+function glInit3Program() {
+    /* Link gl Program */
+    GLD.program = gl.createProgram();
+    gl.attachShader(GLD.program, GLD.vShader);
+    gl.attachShader(GLD.program, GLD.fShader);
+    gl.linkProgram(GLD.program);
+    if(!gl.getProgramParameter(GLD.program, gl.LINK_STATUS)) {
+        throw "program link log: " + gl.getProgramInfoLog(program);
     }
+    /* Start using the gl program */
+    gl.useProgram(GLD.program);
+    /* Release memory used to compile shaders */
+    gl.deleteShader(GLD.vShader);
+    gl.deleteShader(GLD.fShader);
+    gl.detachShader(GLD.program, GLD.vShader);
+    gl.detachShader(GLD.program, GLD.fShader);
+    delete GLD.vShader;
+    delete GLD.fShader;
+    /* Schedule next init function */
+    window.requestAnimationFrame(glInit4VerticesIndices);
 }
-const vBuf = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vBuf);
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-const a_position = gl.getAttribLocation(program, 'a_position');
-/* args: index, size, type, normalized, stride, pointer */
-/* CAUTION! This assumes gl will set .z=0 and .w=1 going from vec2 to vec4 */
-gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(a_position);
 
-/* Arrange the vertices into triangle strips                                 */
-/*                ,+--------------------- 2 vertices to start first triangle */
-/*                |        ,+------------ plus 1 vertex per triangle         */
-/*                |        |          ,+- plus 2 vertices to separate strips */
-const i_per_row = 2 + (columns * 2) + 2;
-/* Note: The -2 is because the triangle strip for the final row does not  */
-/*       need to end with the 2 extra vertices for a degenerate triangle. */
-const indices = new Uint16Array(i_per_row * rows - 2);
-for(let y = 0; y < rows; y ++) {
-    let baseY = y * i_per_row;
-    let iy0 = (y    ) * (columns + 1);
-    let iy1 = (y + 1) * (columns + 1);
-    /* Start first triangle of triangle strip with 2 vertices, then add 1 */
-    /* vertex per triangle for the remaining triangles. This adds 2 per   */
-    /* iteration of the loop because there are two triangles per column.  */
-    /* The triangle pattern tiles like this:                              */
-    /*   0--1--2--3--4   indices to begin first triangle:  0, 5           */
-    /*   | /| /| /| /|   indices to finish first column:   1, 6           */
-    /*   |/ |/ |/ |/ |   indices to define next column:    2, 7           */
-    /*   5--6--7--8--9                                                    */
-    for(let x = 0; x <= columns; x++) {
-        let i = baseY + (x * 2);
-        indices[i] = iy0 + x;
-        indices[i+1] = iy1 + x;
+/* WebGL incremental init chain step 4: Bind vertex and index data */
+function glInit4VerticesIndices() {
+    /* Add an indexed buffer of triangle strips to tile the screen          */
+    /* - NDC coordinates are left-handed: +x=right +y=up +z=into_screen     */
+    /* - Default front-face winding order is counter-clockwise              */
+    /* - Triangle strip winding order is determined by first triangle       */
+    /* - To append disjoint strips A and B into a single buffer, repeat the */
+    /*   last vertex of A and first vertex of B to create a "degenerate"    */
+    /*   triangle that marks the start of a new triangle strip.             */
+    const columns = 240/16;
+    const rows = 160/16;
+    GLD.columns = columns;
+    GLD.rows = rows;
+    GLD.vertices = new Float32Array((columns + 1) * (rows + 1) * 2);
+    for(let y = 0; y <= rows; y++) {
+        for(let x = 0; x <= columns; x++) {
+            let i = ((y * (columns + 1)) + x) * 2;
+            GLD.vertices[i] = x;
+            GLD.vertices[i+1] = y;
+        }
     }
-    /* Make degenerate triangle to separate disjoint triangle strips */
-    if(y + 1 < rows) {
-        let i = baseY + i_per_row - 3;
-        indices[i+1] = indices[i];     /* Duplicate this strip last vertex  */
-        indices[i+2] = iy1;            /* Duplicate next strip first vertex */
+    GLD.vBuf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, GLD.vBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, GLD.vertices, gl.STATIC_DRAW);
+    const a_position = gl.getAttribLocation(GLD.program, 'a_position');
+    /* args: index, size, type, normalized, stride, pointer */
+    /* CAUTION! This assumes gl will set .z=0 and .w=1 going for vec2->vec4 */
+    gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_position);
+
+    /* Arrange the vertices into triangle strips */
+    /* WebGL incremental init chain step 5: Bind index data         */
+    /* Layout of indices for one row:                               */
+    /*   ,+--------------------- 2 vertices to start first triangle */
+    /*   |        ,+------------ plus 1 vertex per triangle         */
+    /*   |        |          ,+- plus 2 vertices to separate strips */
+    /*   2 + (columns * 2) + 2                                      */
+    const i_per_row = 2 + (columns * 2) + 2;
+    /* Note: The -2 is because the triangle strip for the final row does    */
+    /* not need to end with the 2 extra vertices for a degenerate triangle. */
+    GLD.indices = new Uint16Array(i_per_row * rows - 2);
+    for(let y = 0; y < rows; y ++) {
+        let baseY = y * i_per_row;
+        let iy0 = (y    ) * (columns + 1);
+        let iy1 = (y + 1) * (columns + 1);
+        /* Start first triangle of triangle strip with 2 vertices, then */
+        /* add 1 vertex per triangle for the remaining triangles. This  */
+        /* adds 2 per iteration of the loop because there are two       */
+        /* triangles per column. The triangle pattern tiles like this:  */
+        /*   0--1--2--3--4   indices to begin first triangle:  0, 5     */
+        /*   | /| /| /| /|   indices to finish first column:   1, 6     */
+        /*   |/ |/ |/ |/ |   indices to define next column:    2, 7     */
+        /*   5--6--7--8--9                                              */
+        for(let x = 0; x <= columns; x++) {
+            let i = baseY + (x * 2);
+            GLD.indices[i] = iy0 + x;
+            GLD.indices[i+1] = iy1 + x;
+        }
+        /* Make degenerate triangle to separate disjoint triangle strips */
+        if(y + 1 < rows) {
+            let i = baseY + i_per_row - 3;
+            /* Duplicate this strip's last vertex  */
+            GLD.indices[i+1] = GLD.indices[i];
+            /* Duplicate next strip's first vertex */
+            GLD.indices[i+2] = iy1;
+        }
     }
+    GLD.iBuf = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, GLD.iBuf);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, GLD.indices, gl.STATIC_DRAW);
+    /* Schedule next init function */
+    window.requestAnimationFrame(glInit5LoadWasm);
 }
-const iBuf = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuf);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
+/* WebGL incremental init chain step 5: Load Wasm module */
+function glInit5LoadWasm() {
+    /* Draw some stuff */
+    drawTiles();
+    /* Load the wasm module with callback to start the event loop */
+    wasmloadModule(() => {
+        WASM_EXPORT.init();
+        window.requestAnimationFrame(frameZero);  /* Start event loop */
+    });
+}
 
 /* Draw indexed vertices as triangle strips */
 function drawTiles() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLE_STRIP, indices.length, gl.UNSIGNED_SHORT, 0);
+    const length = GLD.indices.length;
+    gl.drawElements(gl.TRIANGLE_STRIP, length, gl.UNSIGNED_SHORT, 0);
 }
 
 /* Draw indexed vertices as a line strip */
 function drawLines() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_SHORT, 0);
+    const length = GLD.indices.length;
+    gl.drawElements(gl.LINE_STRIP, length, gl.UNSIGNED_SHORT, 0);
 }
 
 
@@ -319,7 +363,9 @@ function oddFrame(timestamp_ms) {
 /* Call the wasm module on even frames */
 function evenFrame(timestamp_ms) {
     /* Schedule the next frame */
-    const requestID = window.requestAnimationFrame(oddFrame);
+    if(!PAUSED) {
+        const requestID = window.requestAnimationFrame(oddFrame);
+    }
     /* Compute elapsed time since previous frame in ms, convert to uint32_t */
     const elapsed = timestamp_ms - PREV_TIMESTAMP;
     PREV_TIMESTAMP = timestamp_ms;
@@ -336,6 +382,11 @@ function evenFrame(timestamp_ms) {
 
 /* Handle gamepad connect event */
 function gamepadConn(e) {
+    /* Start the animation loop */
+    if(PAUSED) {
+        PAUSED = false;
+        window.requestAnimationFrame(oddFrame);
+    }
     /* Log gamepad info to help with troubleshooting of button mappings */
     const ix = e.gamepad.index;
     const id = e.gamepad.id;
@@ -369,8 +420,5 @@ function gamepadDisconn(e) {
 window.addEventListener("gamepadconnected", gamepadConn);
 window.addEventListener("gamepaddisconnected", gamepadDisconn);
 
-/* Load the wasm module and start the event loop */
-wasmloadModule(() => {
-    WASM_EXPORT.init();
-    window.requestAnimationFrame(frameZero);  /* Start the event loop */
-});
+/* Start the WebGL canvas initialization chain (split across rAF callbacks) */
+glInit0Clear();
